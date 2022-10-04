f3fb4d6b7cb297ba5ee7d99f3e32667a4bdb83d8
Daniel <raidenandsold@gmail.com>
2022-10-03 19:04:36 -0500

    Add Username entity

--- complete/src/main/java/com/example/springboot/controllers/UserController.java
+++ complete/src/main/java/com/example/springboot/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.entities.Username;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,20 @@ import com.example.springboot.models.User;
 public class UserController {
 
     @GetMapping("/info")
-	public String userInfo(@RequestParam("firstName") String firstName,
-                            @RequestParam("lastName") String lastName,
-                            @RequestParam("email") String email,
-                            @RequestParam("title") String title) {
-        User usr = new User(firstName, lastName, email, title);
+	public String userInfo()
+//			(@RequestParam("firstName") String firstName,
+//                            @RequestParam("lastName") String lastName,
+//                            @RequestParam("email") String email,
+//                            @RequestParam("title") String title)
+		{
+//        User usr = new User(firstName, lastName, email, title);
+		Username username = new Username();
+		username.setUserId(123);
+		username.setFirstName("Daniel");
+		username.setLastName("Vandagriff");
         // User usr2 = new User("Johnson", "reyrey@rey.com", "Mrs.");
-		return usr.getTitle() + " " + usr.getFirstName() + " " + usr.getLastName() + " " + usr.getEmail();
+//		return usr.getTitle() + " " + usr.getFirstName() + " " + usr.getLastName() + " " + usr.getEmail();
+		return username.getUserId() + " " + username.getFirstName() + " " + username.getLastName();
 	}
 
 }
---
+++ complete/src/main/java/com/example/springboot/entities/Username.java
@@ -0,0 +1,30 @@
+package com.example.springboot.entities;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "username")
+public class Username {
+
+    @Id
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "title")
+    private String title;
+}
