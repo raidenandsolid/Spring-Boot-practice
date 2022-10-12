4894a230fcef124d580c51ef6ca11519e1342c25
Daniel Vandagriff <daniel_vandagriff@yahoo.com>
2022-10-12 10:14:04 -0500

    Add Username entity (#4)
    Co-authored-by: Daniel <raidenandsold@gmail.com>

--- complete/src/main/java/com/example/springboot/controllers/UserController.java
+++ complete/src/main/java/com/example/springboot/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.entities.Username;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,12 @@ import com.example.springboot.models.User;
 public class UserController {
 
     @GetMapping("/info")
-	public String userInfo(@RequestParam("firstName") String firstName,
-                            @RequestParam("lastName") String lastName,
-                            @RequestParam("email") String email,
-                            @RequestParam("title") String title) {
-        User usr = new User(firstName, lastName, email, title);
-		return usr.getTitle() + " " + usr.getFirstName() + " " + usr.getLastName() + " " + usr.getEmail();
+	public String userInfo() {
+		Username username = new Username();
+		username.setUserId(123);
+		username.setFirstName("Daniel");
+		username.setLastName("Vandagriff");
+		return username.getUserId() + " " + username.getFirstName() + " " + username.getLastName();
 	}
 
 }
