6f24c410b7e62ba54dc64437c53ab828b1b531f8
Daniel Vandagriff <daniel_vandagriff@yahoo.com>
2022-10-16 14:49:30 -0500

    Connect to database (#5)
    * Add Jdbc support and connect to Oracle DB.
    
    * Delete application.properties
    
    remove file
    
    Co-authored-by: Daniel <raidenandsold@gmail.com>
    Co-authored-by: Vernon Williams <voxenn@gmail.com>

--- complete/src/main/java/com/example/springboot/entities/Username.java
+++ complete/src/main/java/com/example/springboot/entities/Username.java
@@ -2,6 +2,8 @@ package com.example.springboot.entities;
 
 import lombok.Data;
 
+//import net.codejava.SpringJdbcTemplate2OracleApplication;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -27,4 +29,53 @@ public class Username {
 
     @Column(name = "title")
     private String title;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Username{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
